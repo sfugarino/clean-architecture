a6591a9baca10c227ffbc872fb9a45bef6fe6de6
Samuel Fugarino <sfugarino@msn.com>
2022-11-06 16:00:02 -0500

    Initial commit

---
+++ src/Core/BlueMusic.Domain/Entities/Album.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlueMusic.Domain.Entities
+{
+    internal class Album
+    {
+    }
+}
